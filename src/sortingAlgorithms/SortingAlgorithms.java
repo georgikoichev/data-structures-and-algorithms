e0404105880c77b5495a12d3befcf7c6cd98fa6e
georgikoichev <georgikoichev445@gmail.com>
2023-08-24 12:16:31 +0300

    added heap sort

--- src/sortingAlgorithms/SortingAlgorithms.java
+++ src/sortingAlgorithms/SortingAlgorithms.java
@@ -92,9 +92,40 @@ public class SortingAlgorithms {
         }
     }
 
+    public static void heapSort(int[] arr) {
+        int l = arr.length;
+
+        for (int i = l / 2 - 1; i >= 0; i--) {
+            heapify(arr, l, i);
+        }
+
+        for (int i = l - 1; i > 0; i--) {
+            swap(arr, 0, i);
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int l, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < l && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < l && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, l, largest);
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 7, 3, 5, 1, 6, 4};
-        quickSort(arr);
+        heapSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
